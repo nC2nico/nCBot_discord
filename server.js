1930177b4aed5e2c4522406544be41d80d92ec9c
nC2 <93074481+nC2nico@users.noreply.github.com>
2021-12-31 15:34:00 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const querystring = require('querystring');
+const discord = require('discord.js');
+const client = new discord.Client();
+
+http.createServer(function(req, res){
+  if (req.method == 'POST'){
+    var data = "";
+    req.on('data', function(chunk){
+      data += chunk;
+    });
+    req.on('end', function(){
+      if(!data){
+        res.end("No post data");
+        return;
+      }
+      var dataObject = querystring.parse(data);
+      console.log("post:" + dataObject.type);
+      if(dataObject.type == "wake"){
+        console.log("Woke up in post");
+        res.end();
+        return;
+      }
+      res.end();
+    });
+  }
+  else if (req.method == 'GET'){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Discord Bot is active now\n');
+  }
+}).listen(3000);
+
+client.on('ready', message =>{
+  console.log('Bot準備完了～');
+  client.user.setPresence({ activity: { name: 'げーむ' } });
+});
+
+client.on('message', message =>{
+  if (message.author.id == client.user.id || message.author.bot){
+    return;
+  }
+  if(message.isMemberMentioned(client.user)){
+    sendReply(message, "呼びましたか？");
